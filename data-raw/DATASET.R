313766579b46fd5d63ac1185ccc80e86fc24492d
CorradoLanera <corrado.lanera@gmail.com>
2021-03-15 10:42:13 +0100

    import example data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+stopifnot(requireNamespace("here", quietly = TRUE))
+stopifnot(requireNamespace("readr", quietly = TRUE))
+stopifnot(requireNamespace("usethis", quietly = TRUE))
+
+
+dataset_ex <- readr::read_csv(here::here("data-raw", "dataset_ex.csv"))
+
+usethis::use_data(dataset_ex, overwrite = TRUE)
